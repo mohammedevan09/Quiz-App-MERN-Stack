916547de28271d2f151e8302dafbde0dd678e102
Mohammed Evan <121313913+mohammedevan09@users.noreply.github.com>
2023-07-13 14:25:02 +0600

    added some changes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import { config } from 'dotenv'
-import questionRouter from './router/route.js'
+import questionRouter from './routes/route.js'
 import connectDB from './database/connection.js'
 const app = express()
 
